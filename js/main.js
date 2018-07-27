8f4fffb978cff2065050ea09e658fe4644ed7f2a
Mohamed Refaie <mohamed1refaie@hotmail.com>
2018-07-28 00:03:54 +0200

    css and responsive

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoibW9oYW1lZDFyZWZhaWUiLCJhIjoiY2prMmZ0djJtMHJzdTNrbncyZDZ3a3QwbSJ9.WYD5i2HgBXL5A6HVz86UAQ',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
